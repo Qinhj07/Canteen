d92027ab3ec95de6c4d5a0b57b8eb34674e692e7
qinhj11 <qinhj11@chinaunicom.cn>
2023-02-22 14:15:56 +0800

    首次同步提交

---
+++ database/migrations/2023_02_22_093401_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('openid')->nullable();
+            $table->string('phone', 16)->unique();
+            $table->string('name',  64)->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('nickname')->nullable();
+            $table->integer('role')->default(1);
+            $table->integer('status')->default(1);
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2023_02_22_094023_create_munuses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMunusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('price')->default('0');
+            $table->string('introduce')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('package')->default(0)->comment('是否打包0');
+            $table->integer('type')->default(1)->comment('1主食2');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
